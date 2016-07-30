b89d7fb93d1488b4cdc289fecb518fb0d236e915
sergey <seniortuz@gmail.com>
2016-07-30 19:48:54 +0300

    -Autoload class added; -Errors fixed;

--- autoload.php
+++ autoload.php
@@ -1,16 +1,9 @@
 <?php
-	function loader($className) {
-		//echo "Want to load " . $className . "<br>";
-		
-		$mapping = [
-			'Mindk\Blog\Content\Article' => 'vendor/Mindk/Blog/Content/Article.php',
-			'Mindk\Blog\Content\Contact' => 'vendor/Mindk/Blog/Content/Contact.php',
-			'Mindk\Blog\Content\Video' => 'vendor/Mindk/Blog/Content/Video.php',
-			'Mindk\Blog\Category' => 'vendor/Mindk/Blog/Category.php',
-			'Mindk\Blog\Content\Item' => 'vendor/Mindk/Blog/Content/Item.php'
-		];
-
-		require array_key_exists($className, $mapping) ? $mapping[$className] : '';
+	class Autoload {
+		public static function loader($className) {
+			$className = str_replace('\\', '/', $className);
+			require_once 'vendor/' . $className . '.php';
+		}
 	}
 
-	spl_autoload_register('loader');
+	spl_autoload_register(array('Autoload','loader'));
